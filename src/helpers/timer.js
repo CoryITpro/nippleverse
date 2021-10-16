e46ba9b0ec0afa1684155d257c30692532374640
Cory <coryitpro@gmail.com>
2021-10-15 20:23:01 -0400

    Set timer to 15th november 5pm

--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -1,5 +1,5 @@
 export const calculateTimeLeft = () => {
-  const presaleDate = new Date(Date.UTC(2021, 9, 14, 19, 15, 0))
+  const presaleDate = new Date(Date.UTC(2021, 10, 15, 17, 0, 0))
   const difference = presaleDate - new Date()
 
   let timeLeft = {}
